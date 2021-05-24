de48532735107f0410e9959964c5bb5c3df70799
Dylan Newman <39581306+Mesablip@users.noreply.github.com>
2021-05-23 21:45:54 -0700

    Update ec2_initialization.sh

--- ec2_initialization.sh
+++ ec2_initialization.sh
@@ -3,9 +3,9 @@ sudo apt update
 sudo apt install python3-pip -y
 pip install "dask[complete]" --no-cache-dir
 sudo apt install awscli -y
-export AWS_ACCESS_KEY_ID=ASIAV34ILZ5DBGUW4NHE
-export AWS_SECRET_ACCESS_KEY=T2fM2JF+nXVrH/NYrBULuzTkzO9rDGDlVHGGKtFd
-export AWS_SESSION_TOKEN=FwoGZXIvYXdzECoaDJLapQKc43Rcnjp8tyKtAcXR7D2AxZMbiRp94bTG4OUW+va1ZilqQapxzHqwRKM81iDrjqqYwoG3a8ivxs4LtEZfK9xZP/TnEq4z+s/yYRw+X7WVM+t9IJOtGyo920iXB90Z7XDGn+MlbRW6j8C/+X4lkYi4M8+e7B6vUP9uHgbVdfpW5uUyjTZskuVC9/IW6E6qzOk3zSCv9qpNgM7g4xQOUerNAZ1HczcMh5qloGWAkk9DKT8AN3Y1KponKK+roYUGMi0EW9+roQpBs0OX9TJXrmNpN1rOgujf5cfEp1KacA9KFQEFQOJAcVEHtPyrhlM=
+export AWS_ACCESS_KEY_ID=ASIAV34ILZ5DO35U67KX
+export AWS_SECRET_ACCESS_KEY=NbIS44h2TjJfhVuxqKQ7GWCBz27Qbd7wniVpBwWm
+export AWS_SESSION_TOKEN=FwoGZXIvYXdzEF4aDKFGXDZWaL275UuylSKtAcGizkKiCOXOF06iPGmZ7lVfXjW4P00bldxTCsuoGkORUe5D+mkb8diD1y6j6Fc2X3qFBmk6DH2YgNvWp0xbWjViOenfgs2RPuWp3CwgaIz+N+WmvbOjq456WdLReVEJNsja2LwyK6x/PHuTBTi+yfa4X5TlGg11WSLCjXVY1eefJIQeNqR/yMNWWGH9+Sd/OTbvhcFqzk577dn/HYuH0DdUKO/mwZt9cOEDqQ2LKLbcrIUGMi1aW1b3zlEzjMPfnuSkgcenABEMMA8YYSEVTy+8mxvyXvLWzcwQb3mCiqD8rVI=
 aws s3 cp s3://ds102-colbi-scratch/historical_data_2009Q1.txt historical_data_2009Q1.txt
 aws s3 cp s3://ds102-colbi-scratch/historical_data_time_2009Q1.txt historical_data_time_2009Q1.txt
 aws s3 cp s3://ds102-colbi-scratch/feature_prep.py feature_prep.py
