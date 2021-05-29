b46a298d7ad3b17633e45dfa72c61db40d109cb5
Dylan Newman <39581306+Mesablip@users.noreply.github.com>
2021-05-29 00:56:41 -0700

    Update ec2_initialization.sh

--- ec2_initialization.sh
+++ ec2_initialization.sh
@@ -8,5 +8,6 @@ sudo apt install awscli -y
 export AWS_ACCESS_KEY_ID=ASIAV34ILZ5DLTXYU533
 export AWS_SECRET_ACCESS_KEY=GbCzOTSXZG6UlW7dBXCRdnwgbCrNBDXi5Kofym5R
 export AWS_SESSION_TOKEN=FwoGZXIvYXdzENn//////////wEaDMLc6nd1VMZSpiFbDyKtATr3DeIADiR5R0L4M9TQw/HhMnY8EkGjx0vVlwUzGZNkVK/zngVRTrykkI4pPqmvH663DUoMP12PvLPF9GuFy3MgXBdmmhqladv+fVTawHagh4zRWgxZVe+XuHozPHN6QYRIFlfp6g1SU46zrPI5RbewglS3a8qgz0/MHM21hMH9hCesckqa0Ll8z2zdMchAuUUxTt/Z5W2d5ss/Vc9Ui6UCaur329+sKEf7ROtmKOLlx4UGMi09OeWurnOvHCDn0hHsTzo7b29JAts2DRjXLP+ttP+nTiaGZRmhlYnqZDqqMWQ=
+aws s3 cp s3://ds102-colbi-scratch/historical_data_2009Q1.txt historical_data_2009Q1.txt
 aws s3 cp s3://ds102-colbi-scratch/historical_data_time_2009Q1.txt historical_data_time_2009Q1.txt
 aws s3 cp s3://ds102-colbi-scratch/feature_prep.py feature_prep.py
