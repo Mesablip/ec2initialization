dbfe2ef6c398dfe94b16148621708db1eb9bc928
Dylan Newman <39581306+Mesablip@users.noreply.github.com>
2021-05-29 00:54:07 -0700

    Update ec2_initialization.sh

--- ec2_initialization.sh
+++ ec2_initialization.sh
@@ -5,9 +5,8 @@ pip install "dask[complete]" --no-cache-dir
 pip install pyarrow
 pip install dask-ml
 sudo apt install awscli -y
-export AWS_ACCESS_KEY_ID=ASIAV34ILZ5DATJIZ6U7
-export AWS_SECRET_ACCESS_KEY=jvo2qw8AxgTjlehrSH2yh1WTt4arJqwJX1T1OKlt
-export AWS_SESSION_TOKEN=FwoGZXIvYXdzEM7//////////wEaDDph5hHFqH9bG1NwfiKtARSJMVDz48xP+kNUtEwhCJ393g1L+1qEHCoH4+xs2zh5rR7DOLzWHLUkcV1JdxwAQZU7ps7FUB2biAi0grtMeawGEq9gEjjNzAaBsNjV6ICBCcTqNbxpsqylnjqQe8SZDjArFbFQ7OCSDzvkS7ehQa7f42Gnz24+DmqebVyLVkw2DOQjS8ONgMAMBmkWHRo0UWHoYrmIT4Rf8Hxpr+9JcqUA4GFjSetdAJFf1s+9KL6ixYUGMi1Rus6qGFf1AMCeO/D29XupUwXY60uitsAPU/ou7FFpymK6M557bgb/wH4r5I8=
-aws s3 cp s3://ds102-colbi-scratch/historical_data_2009Q1.txt historical_data_2009Q1.txt
+export AWS_ACCESS_KEY_ID=ASIAV34ILZ5DLTXYU533
+export AWS_SECRET_ACCESS_KEY=GbCzOTSXZG6UlW7dBXCRdnwgbCrNBDXi5Kofym5R
+export AWS_SESSION_TOKEN=FwoGZXIvYXdzENn//////////wEaDMLc6nd1VMZSpiFbDyKtATr3DeIADiR5R0L4M9TQw/HhMnY8EkGjx0vVlwUzGZNkVK/zngVRTrykkI4pPqmvH663DUoMP12PvLPF9GuFy3MgXBdmmhqladv+fVTawHagh4zRWgxZVe+XuHozPHN6QYRIFlfp6g1SU46zrPI5RbewglS3a8qgz0/MHM21hMH9hCesckqa0Ll8z2zdMchAuUUxTt/Z5W2d5ss/Vc9Ui6UCaur329+sKEf7ROtmKOLlx4UGMi09OeWurnOvHCDn0hHsTzo7b29JAts2DRjXLP+ttP+nTiaGZRmhlYnqZDqqMWQ=
 aws s3 cp s3://ds102-colbi-scratch/historical_data_time_2009Q1.txt historical_data_time_2009Q1.txt
 aws s3 cp s3://ds102-colbi-scratch/feature_prep.py feature_prep.py
